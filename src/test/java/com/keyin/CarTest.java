6e3cc8d5901f0a8bfbb72ad12c2005f7afd67c35
ashley-colbert <136830986+ashley-colbert@users.noreply.github.com>
2024-05-15 09:58:33 -0230

    Initial commit

---
+++ src/test/java/com/keyin/CarTest.java
@@ -0,0 +1,11 @@
+package com.keyin;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+    @Test
+    public void testIsActiveByDefault() {
+        Assertions.assertTrue(new Car().isActive());
+    }
+}
